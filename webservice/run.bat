0c71db138357f93918fa8c631ccf2d0c21efcc4a
Peter Nagy <peter.nagy@perit.hu>
2021-04-17 13:27:02 +0200

    auditing works at update too

--- webservice/run.bat
+++ webservice/run.bat
@@ -9,6 +9,7 @@ if exist "%launcher%" (
 echo.@echo off>>"%launcher%"
 echo.ConsoleSize 200 20 200 9999>>"%launcher%"
 echo.cd build\install\webservice\bin>>"%launcher%"
+rem echo.set WEBSERVICE_OPTS=-Dspring.profiles.active=default,dev>>"%launcher%"
 echo.call webservice.bat>>"%launcher%"
 
 start cmd /c "%launcher%"
