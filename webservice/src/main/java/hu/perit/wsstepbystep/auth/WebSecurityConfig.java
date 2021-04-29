7479d024c114bef834772df58fa0d9ec821c4625
Peter Nagy <peter.nagy@perit.hu>
2021-04-29 21:31:36 +0200

    SimpleKeycloakWebSecurityConfigurerAdapter

--- webservice/src/main/java/hu/perit/wsstepbystep/auth/WebSecurityConfig.java
+++ webservice/src/main/java/hu/perit/wsstepbystep/auth/WebSecurityConfig.java
@@ -16,19 +16,8 @@
 
 package hu.perit.wsstepbystep.auth;
 
-import org.keycloak.adapters.KeycloakConfigResolver;
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
-import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
-import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticatedActionsFilter;
-import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
-import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter;
-import org.keycloak.adapters.springsecurity.filter.KeycloakSecurityContextRequestFilter;
-import org.keycloak.adapters.springsecurity.management.HttpSessionManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
@@ -36,15 +25,11 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.web.authentication.logout.LogoutFilter;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
 import org.springframework.security.web.session.SessionManagementFilter;
 
 import hu.perit.spvitamin.spring.rest.api.AuthApi;
 import hu.perit.spvitamin.spring.security.auth.SimpleHttpSecurityBuilder;
+import hu.perit.spvitamin.spring.security.keycloak.SimpleKeycloakWebSecurityConfigurerAdapter;
 import hu.perit.wsstepbystep.rest.api.AuthorApi;
 import hu.perit.wsstepbystep.rest.api.BookApi;
 import lombok.extern.slf4j.Slf4j;
@@ -65,9 +50,8 @@ public class WebSecurityConfig
      */
     @KeycloakConfiguration
     @Order(1)
-    public static class Order1 extends KeycloakWebSecurityConfigurerAdapter
+    public static class Order1 extends SimpleKeycloakWebSecurityConfigurerAdapter
     {
-
         /**
          * This is a global configuration, will be applied to all oder configurer adapters
          *
@@ -84,105 +68,8 @@ public class WebSecurityConfig
         @Override
         protected void configure(HttpSecurity http) throws Exception
         {
-            // According documentation
-            //            super.configure(http);
-            //            http.authorizeRequests() //
-            //                .antMatchers(AuthApi.BASE_URL_AUTHENTICATE).fullyAuthenticated() //
-            //                .anyRequest().permitAll();
-
-
-            http.requestMatchers() //
-                .antMatchers(AuthApi.BASE_URL_AUTHENTICATE + "/**").and() //
-                .csrf().requireCsrfProtectionMatcher(keycloakCsrfRequestMatcher()) //
-                .and() //
-                .sessionManagement() //
-                .sessionAuthenticationStrategy(sessionAuthenticationStrategy()) //
-                .and() //
-                .addFilterBefore(keycloakPreAuthActionsFilter(), LogoutFilter.class) //
-                .addFilterBefore(keycloakAuthenticationProcessingFilter(), LogoutFilter.class) //
-                .addFilterAfter(keycloakSecurityContextRequestFilter(), SecurityContextHolderAwareRequestFilter.class) //
-                .addFilterAfter(keycloakAuthenticatedActionsRequestFilter(), KeycloakSecurityContextRequestFilter.class) //
-                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint()) //
-                .and() //
-                .logout() //
-                .addLogoutHandler(keycloakLogoutHandler()) //
-                .logoutUrl("/sso/logout").permitAll() //
-                .logoutSuccessUrl("/").and() //
-                .authorizeRequests() //
-                .anyRequest().fullyAuthenticated();
-
-            // Spvitamin-style
-            //            SimpleHttpSecurityBuilder.newInstance(http) //
-            //                .scope(AuthApi.BASE_URL_AUTHENTICATE + "/**") //
-            //                .basicAuth();
-        }
-
-
-        @Override
-        @Bean
-        protected SessionAuthenticationStrategy sessionAuthenticationStrategy()
-        {
-            return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-        }
-
-
-        @Bean
-        @Override
-        @ConditionalOnMissingBean(HttpSessionManager.class)
-        protected HttpSessionManager httpSessionManager()
-        {
-            return new HttpSessionManager();
-        }
-
-
-        /*
-         * By Default, the Spring Security Adapter looks for a keycloak.json configuration file. You can make sure it 
-         * looks at the configuration provided by the Spring Boot Adapter
-         */
-        @Bean
-        public KeycloakConfigResolver keycloakConfigResolver()
-        {
-            return new KeycloakSpringBootConfigResolver();
-        }
-
-        // Needed because Spring Boot eagerly registers filter beans to the web application context. This prevents them being registered twice.
-        @Bean
-        public FilterRegistrationBean<KeycloakAuthenticationProcessingFilter> keycloakAuthenticationProcessingFilterRegistrationBean(
-            KeycloakAuthenticationProcessingFilter filter)
-        {
-            FilterRegistrationBean<KeycloakAuthenticationProcessingFilter> registrationBean = new FilterRegistrationBean<>(filter);
-            registrationBean.setEnabled(false);
-            return registrationBean;
-        }
-
-        // Needed because Spring Boot eagerly registers filter beans to the web application context. This prevents them being registered twice.
-        @Bean
-        public FilterRegistrationBean<KeycloakPreAuthActionsFilter> keycloakPreAuthActionsFilterRegistrationBean(
-            KeycloakPreAuthActionsFilter filter)
-        {
-            FilterRegistrationBean<KeycloakPreAuthActionsFilter> registrationBean = new FilterRegistrationBean<>(filter);
-            registrationBean.setEnabled(false);
-            return registrationBean;
-        }
-
-        // Needed because Spring Boot eagerly registers filter beans to the web application context. This prevents them being registered twice.
-        @Bean
-        public FilterRegistrationBean<KeycloakAuthenticatedActionsFilter> keycloakAuthenticatedActionsFilterBean(
-            KeycloakAuthenticatedActionsFilter filter)
-        {
-            FilterRegistrationBean<KeycloakAuthenticatedActionsFilter> registrationBean = new FilterRegistrationBean<>(filter);
-            registrationBean.setEnabled(false);
-            return registrationBean;
-        }
-
-        // Needed because Spring Boot eagerly registers filter beans to the web application context. This prevents them being registered twice.
-        @Bean
-        public FilterRegistrationBean<KeycloakSecurityContextRequestFilter> keycloakSecurityContextRequestFilterBean(
-            KeycloakSecurityContextRequestFilter filter)
-        {
-            FilterRegistrationBean<KeycloakSecurityContextRequestFilter> registrationBean = new FilterRegistrationBean<>(filter);
-            registrationBean.setEnabled(false);
-            return registrationBean;
+            scope(http, AuthApi.BASE_URL_AUTHENTICATE + "/**");
+            configureKeycloak(http);
         }
     }
 
