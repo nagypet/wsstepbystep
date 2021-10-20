196bb5e776a3d2c401816e7f67a84aaa58cf417b
Peter Nagy <peter.nagy@perit.hu>
2021-10-20 09:43:57 +0200

    Step5 changed: ResourceNotFoundException is checked now

--- webservice/src/main/java/hu/perit/wsstepbystep/rest/api/BookController.java
+++ webservice/src/main/java/hu/perit/wsstepbystep/rest/api/BookController.java
@@ -49,8 +49,7 @@ public class BookController implements BookApi
     // getBookById
     //------------------------------------------------------------------------------------------------------------------
     @Override
-    public BookDTO getBookById(Long id) throws ResourceNotFoundException
-    {
+    public BookDTO getBookById(Long id) throws ResourceNotFoundException {
         log.debug("getBookById()");
         
         if (LongUtils.equals(id, 120L))
